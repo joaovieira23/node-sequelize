b64f32c2c5cb8c247cebb7649734cbc42ca4d81b
João Victor Vieira de Andrade <joaovictorvieira.23@hotmail.com>
2020-02-15 13:41:20 -0300

    Associacao entre models

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ const routes = express.Router();
 routes.get('/users', UserController.index);
 routes.post('/users', UserController.store);
 
+routes.get('/users/:user_id/addresses', AddressController.index);
 routes.post('/users/:user_id/addresses', AddressController.store)
 
 module.exports = routes;
